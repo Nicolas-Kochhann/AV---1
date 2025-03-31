c1bae8c8a50d0532bc34754875298b337ec25e44
LuizHenriqueMP <98365070+LuizHenriqueMP@users.noreply.github.com>
2025-03-31 15:34:59 -0300

    Risco de queda de energia
    socorro

--- page-2/script.js
+++ page-2/script.js
@@ -5,8 +5,20 @@ formulario.addEventListener("submit", function(event){
     let dados = Object.fromEntries(formData.entries());
 
     let texto = document.getElementById("vencedor");
-    
+
     if(dados['mao1']==dados['mao2']){
         texto.textContent = `VENCEDOR: EMPATE`;
+    }else if(dados['mao1']=="pedra" && dados['mao2']=="tesoura"){
+        texto.textContent = `VENCEDOR: Jogador 1`;
+    }else if(dados['mao1']=="papel" && dados['mao2']=="pedra"){
+        texto.textContent = `VENCEDOR: Jogador 1`;
+    }else if(dados['mao1']=="tesoura" && dados['mao2']=="papel"){
+        texto.textContent = `VENCEDOR: Jogador 1`;
+    }else if(dados['mao2']=="pedra" && dados['mao1']=="tesoura"){
+        texto.textContent = `VENCEDOR: Jogador 2`;
+    }else if(dados['mao2']=="papel" && dados['mao1']=="pedra"){
+        texto.textContent = `VENCEDOR: Jogador 2`;
+    }else if(dados['mao2']=="tesoura" && dados['mao1']=="papel"){
+        texto.textContent = `VENCEDOR: Jogador 2`;
     }
 });
